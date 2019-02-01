f7eb3001f19328a067ce6e655ce1c4cbe5e4c858
kaka <1198560793@qq.com>
2019-02-01 14:45:50 +0800

    '视频播放'

--- src/views/Kecheng/test.vue
+++ src/views/Kecheng/test.vue
@@ -38,8 +38,10 @@ export default {
                 aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
                 fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
                 sources: [{
-                type: "",
-                src: "" //url地址
+                    type: "video/ogg",
+                    type:"video/webm",
+                    type: "video/mp4",
+                    src: "" //url地址
                 }],
                 poster: "../../assets/img/share.jpg", //你的封面地址
                 // width: document.documentElement.clientWidth,
